a0c8061783ad606f946714fdec2d649e683bc631
Rodolfo Campos <camposer@gmail.com>
2016-04-07 20:18:32 +0200

    Implementación CRUD+ con jQuery

--- ejercicio3/public/producto/js/productoUtil.js
+++ ejercicio3/public/producto/js/productoUtil.js
@@ -16,14 +16,20 @@ function ProductoUtil() {
 		}).done(callback);
 	}
 
-	this.eliminar = function(id) {
-
+	this.eliminar = function(id, callback) {
+		$.ajax(URL_BASE + '/' + id, {
+			type: 'delete'
+		}).done(callback);
 	}
 
-	this.obtener = function(id) {
+	this.obtener = function(id, callback) {
+		$.ajax(URL_BASE + '/' + id, {
+			type: 'get',
+			dataType: 'json'
+		}).done(callback);		
 	}
 
-	this.modificar = function(p) {
+	this.modificar = function(p, callback) {
 		$.ajax(URL_BASE + '/' + p.id, {
 			type: 'put',
 			data: JSON.stringify(p),
